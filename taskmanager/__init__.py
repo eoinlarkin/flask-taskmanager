0b9c0027e9d24f3c86d60ee8aa1439f02a33e1fc
eoinlarkin <eoin.larkin@gmail.com>
2022-05-17 21:43:37 +0100

    chore: fix heroku config

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -13,15 +13,14 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
-if os.environ.get("DEVELOPMENT") == True: # working with local database
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")  # local
 else:
     uri = os.environ.get("DATABASE_URL")
     if uri.startswith("postgres://"):
-        uri = uri.replace("postgres://", "postgresql://",1)
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
 
-# setting SQL Alchemy database to the instance of our Flask app
 db = SQLAlchemy(app)
 
-from taskmanager import routes
+from taskmanager import routes  # noqa
