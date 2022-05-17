ded60900d3e6f2c05c9dcfb7f43e128f77b0a02b
eoinlarkin <eoin.larkin@gmail.com>
2022-05-17 11:30:36 +0100

    feat: add ability to post from form to database

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -9,13 +9,15 @@ def home():
 
 @app.route("/categories")
 def categories():
-    return render_template("categories.html")
+    # this will query the database and return all records from this table
+    categories =  list(Category.query.order_by(Category.category_name).all())
+    return render_template("categories.html", categories=categories)
 
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
         if request.method == "POST":
             category = Category(category_name=request.form.get("category_name"))
-            db.session.add()
+            db.session.add(category)
             db.session.commit()
             return redirect(url_for("categories"))
         return render_template("add_category.html")
